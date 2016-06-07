06a3553ad77bee16ec112d330590c4b13c179112
hujiulong <me@hujiulong.com>
2016-06-07 12:26:41 +0800

    uodate

--- js/index-b.js
+++ js/index-b.js
@@ -26,6 +26,7 @@
 	//队伍数据数组,会保存每个队伍的模型和信息(如血量)
 	var teamsData = [];
 	var sphereMaterial;
+	var particleSystems = [];
 
 
 	init();
@@ -123,7 +124,109 @@
 		scene.add( makeSkybox( urls, 50000 ) );
 
 
+	function initparticleSystems(){
 
+		var canvas1 = document.createElement( 'canvas' );
+		canvas1.width = 16;
+		canvas1.height = 16;
+
+		var materials = [];
+
+		var context1 = canvas1.getContext( '2d' );
+		var gradient1 = context1.createRadialGradient( canvas1.width / 2, canvas1.height / 2, 0, canvas1.width / 2, canvas1.height / 2, canvas1.width / 2 );
+		gradient1.addColorStop( 0, 'rgba(255,0,0,1)' );
+		gradient1.addColorStop( 0.2, 'rgba(255,10,10,1)' );
+		gradient1.addColorStop( 0.4, 'rgba(64,0,0,1)' );
+		gradient1.addColorStop( 1, 'rgba(0,0,0,1)' );
+
+		context1.fillStyle = gradient1;
+		context1.fillRect( 0, 0, canvas1.width, canvas1.height );
+
+		var material = new THREE.SpriteMaterial( {
+				map: new THREE.CanvasTexture( canvas1 ),
+				blending: THREE.AdditiveBlending
+			});
+		materials.push(material);
+
+		var canvas2 = document.createElement( 'canvas' );
+		canvas2.width = 16;
+		canvas2.height = 16;
+		var context2 = canvas2.getContext( '2d' );
+		var gradient2 = context2.createRadialGradient( canvas2.width / 2, canvas2.height / 2, 0, canvas2.width / 2, canvas2.height / 2, canvas2.width / 2 );
+		
+		
+		gradient2.addColorStop( 0, 'rgba(0,0,210,1)' );
+		gradient2.addColorStop( 0.2, 'rgba(10,10,210,1)' );
+		gradient2.addColorStop( 0.4, 'rgba(0,0,64,1)' );
+		gradient2.addColorStop( 1, 'rgba(0,0,0,1)' );
+
+
+		context2.fillStyle = gradient2;
+		context2.fillRect( 0, 0, canvas2.width, canvas2.height );
+
+		var material = new THREE.SpriteMaterial( {
+				map: new THREE.CanvasTexture( canvas2 ),
+				blending: THREE.AdditiveBlending
+			});
+		materials.push(material);
+
+		console.log(materials);
+
+		for(var i=0;i<20;i++){
+			var particleArr = {
+				available : true,
+				particles : []
+			};
+			for(var j=0;j<60;j++){
+				var particle = new THREE.Sprite( materials[i%2] );
+				particle.pos = 0;
+				particle.visible = false;
+				particleArr.particles.push(particle);
+				scene.add(particle);
+			}
+			particleSystems.push(particleArr);
+		}
+	}
+
+
+
+	function startParticles(curve ,time) {
+		for(var i=0;i<particleSystems.length;i++){
+			if(particleSystems[i].available){
+
+				var particleSystem = particleSystems[i];
+				particleSystem.available = false;
+
+				var firstParticle = particleSystem.particles[0];
+				var lastParticle  = particleSystem.particles[particleSystem.particles.length-1];
+
+				for(var i=0;i<particleSystem.particles.length;i++){
+
+					var particle = particleSystem.particles[i];
+					particle.visible = true;
+					particle.scale.set(60-i,60-i,0);
+					new TWEEN.Tween( particle )
+						.delay( i*10 )
+						.to( {pos:1}, 1800 )
+						.onUpdate(function(){
+							this.position.copy(curve.getPointAt(this.pos));
+						})
+						.onComplete(function(){
+							this.pos = 0;
+							this.visible = false;
+							if(this === firstParticle){
+								
+							}else if(this === lastParticle){
+								particleSystem.available = true;
+							}			
+						})
+						.start();
+				}
+				break;
+			}
+		}
+
+	}
 
 		// Lights
 
@@ -140,6 +243,31 @@
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		container.appendChild( renderer.domElement );
 
+		initparticleSystems();
+
+		var orbit = new THREE.EllipseCurve(
+						0,  0,            
+						200, 200,          
+						0,  2*Math.PI,  
+						false,            
+						0                
+					);
+
+		var startPos = new THREE.Vector3(-1000,-1500,0);
+		var endPos = new THREE.Vector3(orbit.getPointAt(0.2).x,orbit.getPointAt(0.2).y,0);
+		var dx = endPos.x-startPos.x;
+		var dy = endPos.y-startPos.y;
+		var d = 1000;
+		var curve =	 new THREE.SplineCurve3([
+							    new THREE.Vector3(startPos.x, startPos.y, 150),
+							    new THREE.Vector3(startPos.x + dx/3, startPos.y + dy/3, d/4), 
+							    new THREE.Vector3(endPos.x - dx/3, endPos.y - dy/3, d/4),
+							    new THREE.Vector3(endPos.x, endPos.y, 50)
+							]);
+
+		var func = function() { startParticles(curve,2000); }
+		setInterval(func,2000);
+		
 		var geometry = new THREE.SphereGeometry( 300, 32, 16 );
 		sphereMaterial = new THREE.MeshBasicMaterial( { color: 0x00aaff,  shininess: 10, opacity: 0.3, transparent: true } );
 		var mesh = new THREE.Mesh( geometry, sphereMaterial );
@@ -189,13 +317,7 @@
 					});
 					serviceMesh.scale.set(0.6,0.6,0.6);
 					serviceMesh.position.add(serviceOffset);
-					var orbit = new THREE.EllipseCurve(
-						0,  0,            
-						200, 200,          
-						0,  2*Math.PI,  
-						false,            
-						0                
-					);
+
 					var pos = 0;
 					for(var i=0;i<5;i++){
 						var service = serviceMesh.clone();
@@ -240,7 +362,7 @@
 
 	function render() {
 		var timer = 0.0001 * Date.now();
-		sphereMaterial.opacity = 0.3 + 0.3 * Math.sin( 15 * timer );
+		// sphereMaterial.opacity = 0.3 + 0.3 * Math.sin( 15 * timer );
 		// console.log(sphereMaterial);
 		controls.update();
 		TWEEN.update();
